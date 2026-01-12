299fa36dcba4a9d1cdb314197cae250d69604dbc
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-12 14:24:06 +0000

    incluído

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { ProjectsPageComponent } from './pages/projects/projects-page.component';
 import { AboutPageComponent } from './pages/about/about-page.component';
 import { ContactPageComponent } from './pages/contact/contact-page.component';

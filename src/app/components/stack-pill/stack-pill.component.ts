a5c6717eb49f9c77b7bc2235b3c076361af7fa9e
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-14 17:29:44 +0000

    otimizados

--- src/app/components/stack-pill/stack-pill.component.ts
+++ src/app/components/stack-pill/stack-pill.component.ts
@@ -1,24 +1,21 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { TAG_CONFIG, StackPillData } from '../../constants/project-tags.config';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { TAG_CONFIG, PillCategory } from '../../constants/project-tags.config';
 
 @Component({
   selector: 'app-stack-pill',
-  standalone: true,
   templateUrl: './stack-pill.component.html',
   styleUrls: ['./stack-pill.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    '[attr.data-category]': 'data().category'
+    '[attr.data-category]': 'category()'
   }
 })
 export class StackPillComponent {
   tag = input.required<string>();
 
-  data(): StackPillData {
-    return TAG_CONFIG[this.tag] ?? {
-      label: this.tag,
-      iconPath: '',
-      category: 'frontend',
-    };
-  }
+  pill = computed(() => TAG_CONFIG[this.tag()]);
+
+  label = computed(() => this.pill()?.label ?? this.tag());
+  iconPath = computed(() => this.pill()?.iconPath);
+  category = computed<PillCategory>(() => this.pill()?.category ?? 'frontend');
 }

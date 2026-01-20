863cbbe0c2775befd4fe00b409b3389412f4e88c
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-20 16:34:07 +0000

    OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoaderComponent } from './loader/loader.component';
 import { LoaderService } from './core/services/loader.service';
-import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, LoaderComponent],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    LoaderComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  loading = this.loader.loading$;
+  loading$;
 
-  constructor(private loader: LoaderService) {}
+  constructor(private loader: LoaderService) {
+    this.loading$ = this.loader.loading$;
+    this.loader.show();
+    setTimeout(() => {
+      this.loader.hide();
+    }, 1200);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { LoaderInterceptor } from './core/interceptors/loader-interceptor';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi()),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: LoaderInterceptor,
-      multi: true,
-    },
-  ],
+    provideHttpClient() // SEM interceptor
+  ]
 };

35b6d6b4b9fb18c03f7b6cd1c542bf1703ddde9e
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-29 19:49:30 +0000

    identado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -28,47 +28,46 @@ export const routes: Routes = [
           ),
         data: { cosmic: 'minimal' },  
       },
-    
-    {
-      path: 'about',
+      {
+        path: 'about',
+          loadComponent: () =>
+            import('./pages/about/about-page.component').then(
+              (m) => m.AboutPageComponent
+            ),
+          data: { cosmic: 'silent' },
+      },
+      {
+        path: 'Ano2024',
+          loadComponent: () =>
+            import('./components/footer/pages/Ano2024/Ano-2024.component').then(
+              (m) => m.Year2024Component
+            ),
+          data: { cosmic: 'silent' },
+      },
+      {
+        path: 'Background',
         loadComponent: () =>
-          import('./pages/about/about-page.component').then(
-            (m) => m.AboutPageComponent
+          import('./components/footer/pages/background/background.component').then(
+            (m) => m.BackgroundComponent
           ),
         data: { cosmic: 'silent' },
-    },
-    {
-      path: 'Ano2024',
-        loadComponent: () =>
-          import('./components/footer/pages/Ano2024/Ano-2024.component').then(
-            (m) => m.Year2024Component
-         ),
-        data: { cosmic: 'silent' },
-    },
-    {
-      path: 'Background',
-      loadComponent: () =>
-        import('./components/footer/pages/background/background.component').then(
-          (m) => m.BackgroundComponent
-        ),
-        data: { cosmic: 'silent' },
-    },
-    {
-      path: 'Domisdev-v1.0',
-        loadComponent: () =>
-          import('./components/footer/pages/domisdev/domisdev-1.0.component').then(
-            (m) => m.DomisdevV1Component
-          ),
-        data: { cosmic: 'silent' },
-    },
-    {
-      path: 'privacidade',
-        loadComponent: () =>
-          import('./components/footer/pages/privacidade/privacidade.component').then(
-            (m) => m.PrivacidadeComponent
-          ),
-        data: { cosmic: 'silent' },
-    }
+      },
+      {
+        path: 'Domisdev-v1.0',
+          loadComponent: () =>
+            import('./components/footer/pages/domisdev/domisdev-1.0.component').then(
+              (m) => m.DomisdevV1Component
+            ),
+          data: { cosmic: 'silent' },
+      },
+      {
+        path: 'privacidade',
+          loadComponent: () =>
+            import('./components/footer/pages/privacidade/privacidade.component').then(
+              (m) => m.PrivacidadeComponent
+            ),
+          data: { cosmic: 'silent' },
+      }
     ],  
   }  
 ];

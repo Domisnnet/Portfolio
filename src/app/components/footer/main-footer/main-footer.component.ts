502f002724f0a3044e007ba21cd94e2b1918131b
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-29 16:58:44 +0000

    transferidos

---
+++ src/app/components/footer/main-footer/main-footer.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-main-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main-footer.component.html',
+  styleUrls: ['./main-footer.component.scss']
+})
+export class MainFooterComponent {
+  status = 'SYSTEM ONLINE';
+  routeLabel = 'ORBIT STABLE';
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(event => {
+        this.updateStatus((event as NavigationEnd).urlAfterRedirects);
+      });
+  }
+
+  private updateStatus(url: string) {
+    if (url.includes('projects')) {
+      this.routeLabel = 'SCANNING SECTORS';
+    } else if (url.includes('contact')) {
+      this.routeLabel = 'OPEN CHANNEL';
+    } else {
+      this.routeLabel = 'ORBIT STABLE';
+    }
+  }
+}

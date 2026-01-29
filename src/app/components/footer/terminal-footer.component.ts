274755ccf61db0afc49c20e84e93983733c07bbc
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-29 16:22:36 +0000

    renomeado

---
+++ src/app/components/footer/terminal-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-terminal-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './terminal-footer.component.html',
+  styleUrls: ['./terminal-footer.component.scss']
+})
+export class TerminalFooterComponent {}

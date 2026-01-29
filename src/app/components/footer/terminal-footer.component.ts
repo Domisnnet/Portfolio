d96de813b364f151de1247e326c35de75954af4c
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-01-29 16:46:14 +0000

    ajustado

--- src/app/components/footer/terminal-footer.component.ts
+++ src/app/components/footer/terminal-footer.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-terminal-footer',
@@ -8,4 +10,25 @@ import { CommonModule } from '@angular/common';
   templateUrl: './terminal-footer.component.html',
   styleUrls: ['./terminal-footer.component.scss']
 })
-export class TerminalFooterComponent {}
+export class TerminalFooterComponent {
+  status = 'SYSTEM ONLINE';
+  routeLabel = 'ORBIT STABLE';
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(event => {
+        this.updateStatus((event as NavigationEnd).urlAfterRedirects);
+      });
+  }
+
+  private updateStatus(url: string) {
+    if (url.includes('projects')) {
+      this.routeLabel = 'SCANNING SECTORS';
+    } else if (url.includes('contact')) {
+      this.routeLabel = 'OPEN CHANNEL';
+    } else {
+      this.routeLabel = 'ORBIT STABLE';
+    }
+  }
+}

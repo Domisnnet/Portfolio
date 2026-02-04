7b66feaa1e682ae0e39682ae5f6ebb02a510fba9
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-02-04 11:23:44 +0000

    adicionado

---
+++ src/app/cosmic/components/layer-indicator/layer-indicator.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layer-indicator',
+  standalone: true,
+  templateUrl: './layer-indicator.component.html',
+  styleUrls: ['./layer-indicator.component.scss']
+})
+export class LayerIndicatorComponent {}

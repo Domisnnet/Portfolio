d58ffee16fae15954c88c87e032c7251ddd6de82
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-02-04 11:37:07 +0000

    ajustado

--- src/app/cosmic/layout/cosmic-section/cosmic-section.component.ts
+++ src/app/cosmic/layout/cosmic-section/cosmic-section.component.ts
@@ -1,8 +1,11 @@
 import { Component, computed } from '@angular/core';
 import { cosmicLayer } from '../../state/cosmic-layer-signal';
+import { LayerIndicatorComponent } from '@app/cosmic/components/layer-indicator/layer-indicator.component';
 
 @Component({
   selector: 'app-cosmic-section',
+  standalone: true,
+  imports: [LayerIndicatorComponent],
   templateUrl: './cosmic-section.component.html',
   styleUrls: ['./cosmic-section.component.scss']
 })

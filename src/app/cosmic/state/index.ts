d93f13b53da2a01b7e2671870a543396e872073f
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-02-05 18:35:01 +0000

    ajustado

--- src/app/cosmic/state/index.ts
+++ src/app/cosmic/state/index.ts
@@ -1,4 +1,3 @@
 export * from '@app/cosmic/state/cosmic-layer-service';
 export * from '@app/cosmic/state/cosmic-layer-signal';
 export * from '@app/cosmic/state/cosmic-layer-types';
-export * from '@app/core/services/cosmic-effects.service';

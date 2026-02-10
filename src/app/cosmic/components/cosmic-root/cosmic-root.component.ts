f6a1fe8d7c1c4576ad6d7792d53ea16f0263706b
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-02-10 11:58:07 +0000

    adicionado

--- src/app/cosmic/components/cosmic-root/cosmic-root.component.ts
+++ src/app/cosmic/components/cosmic-root/cosmic-root.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { CosmicStarsComponent } from '@app/cosmic/components/cosmic-stars/cosmic-stars.component';
+import { CosmicNebulaComponent } from '@app/cosmic/components/cosmic-nebula/cosmic-nebula.component';
 import { CosmicGridComponent } from '@app/cosmic/components/cosmic-grid/cosmic-grid.component';
 import { CosmicBeamComponent } from '@app/cosmic/components/cosmic-beam/cosmic-beam.component';
 import { LayerJumpComponent } from '@app/cosmic/components/layer-jump/layer-jump.component';
+import { LayerIndicatorComponent } from '../layer-indicator/layer-indicator.component';
 import { WormholeComponent } from '../wormhole/wormhole.component';
 
 @Component({
@@ -10,10 +12,12 @@ import { WormholeComponent } from '../wormhole/wormhole.component';
   standalone: true,
   imports: [
     CosmicStarsComponent,
+    CosmicNebulaComponent,
     CosmicGridComponent,
     CosmicBeamComponent,
     LayerJumpComponent,
-    WormholeComponent,
+    LayerIndicatorComponent,
+    WormholeComponent
   ],
   templateUrl: './cosmic-root.component.html',
   styleUrls: ['./cosmic-root.component.scss'],

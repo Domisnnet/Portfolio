acbd3b10291050aeb38e3bacb325d6ad9de7f462
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-02-10 14:06:58 +0000

    ok

--- dist/portfolio/browser/pages/background/background.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-background',
-  templateUrl: './background.component.html',
-  styleUrls: ['./background.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class BackgroundComponent {}

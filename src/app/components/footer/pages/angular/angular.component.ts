980e981f5cc9677c7d35d185f108ac447bc22c1e
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-02-19 17:24:53 +0000

    ajustado

--- src/app/components/footer/pages/angular/angular.component.ts
+++ src/app/components/footer/pages/angular/angular.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-angular',
@@ -8,5 +9,23 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './angular.component.html',
   styleUrls: ['./angular.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+
+  animations: [
+    trigger('cardEnter', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+          transform: 'translateY(16px) scale(0.98)'
+        }),
+        animate(
+          '320ms cubic-bezier(.2,.8,.2,1)',
+          style({
+            opacity: 1,
+            transform: 'translateY(0) scale(1)'
+          })
+        )
+      ])
+    ])
+  ]
 })
 export class AngularComponent {}

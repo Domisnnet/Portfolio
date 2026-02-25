d66fb9c6e457822b0538e6669c9f2f18c8fe4b36
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-02-25 18:32:36 +0000

    ok

--- dist/portfolio/browser/pages/access/access.component.ts
+++ dist/portfolio/browser/pages/access/access.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { cardEnterAnimation } from '@app/components/card/card.animations';
 
 @Component({
   selector: 'app-access',
@@ -8,5 +9,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './access.component.html',
   styleUrl: './access.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [ cardEnterAnimation ]
 })
 export class AccessComponent {}
--- dist/portfolio/browser/pages/laboratory/lab.component.ts
+++ dist/portfolio/browser/pages/laboratory/lab.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { cardEnterAnimation } from '@app/components/card/card.animations';
 
 @Component({
   selector: 'app-lab',
@@ -8,5 +9,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './lab.component.html',
   styleUrl: './lab.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [ cardEnterAnimation ]
 })
 export class LabComponent {}

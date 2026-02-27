dd86caae008fc9a5ce3b854abac4d27b4594594e
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-02-27 12:40:37 +0000

    ajustado e refatorado para botão padrão

--- src/app/pages/contact/contact-page.component.ts
+++ src/app/pages/contact/contact-page.component.ts
@@ -1,53 +1,41 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 import { FormsModule } from '@angular/forms';
-import { trigger, transition, style, animate } from '@angular/animations';
+import { ButtonComponent } from '@app/components/button/button.component';
+import { cardEnterAnimation } from '@app/components/card/card.animations';
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    ButtonComponent
   ],
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [
-    trigger('cardEnter', [
-      transition(':enter', [
-        style({
-          opacity: 0,
-          transform: 'translateY(12px)'
-        }),
-        animate(
-          '280ms cubic-bezier(.2,.8,.2,1)',
-          style({
-            opacity: 1,
-            transform: 'none'
-          })
-        )
-      ])
-    ])
-  ]
+  animations: [ cardEnterAnimation ]
 })
 export class ContactPageComponent {
-  private firestore: Firestore = inject(Firestore);
-  
-  contactData = {
-    name: '',
-    email: '',
-    message: ''
-  };
+  private firestore = inject(Firestore);
+  isSubmitting = false; 
+  contactData = { name: '', email: '', message: '' };
 
   async onSubmit() {
+    if (this.isSubmitting) return; 
+    this.isSubmitting = true;
+
     try {
       const contactsCollection = collection(this.firestore, 'contacts');
       await addDoc(contactsCollection, this.contactData);
       alert('Mensagem salva com sucesso!');
       this.contactData = { name: '', email: '', message: '' };
+
     } catch (err) {
       console.error('Erro ao salvar mensagem:', err);
       alert('Ocorreu um erro, tente novamente.');
+    } finally {
+      this.isSubmitting = false; 
     }
   }
 }

413eebfcff87f4433e3b02a7a8620ff6a2f4d558
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2026-03-20 18:03:06 +0000

    ok

--- src/app/cosmic/components/cosmic-root/cosmic-root.component.ts
+++ src/app/cosmic/components/cosmic-root/cosmic-root.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
 import { ScanningArtifactComponent } from '../scanning-artifact/scanning-artifact.component';
 import { CosmicStarsComponent } from '@app/cosmic/components/cosmic-stars/cosmic-stars.component';
-import { SecretStarComponent } from '../secret-star/secret-star.component';
 import { CosmicNebulaComponent } from '@app/cosmic/components/cosmic-nebula/cosmic-nebula.component';
 import { CosmicGridComponent } from '@app/cosmic/components/cosmic-grid/cosmic-grid.component';
 import { CosmicBeamComponent } from '@app/cosmic/components/cosmic-beam/cosmic-beam.component';
-import { LayerJumpComponent } from '@app/cosmic/components/layer-jump/layer-jump.component';
-import { LayerIndicatorComponent } from '../layer-indicator/layer-indicator.component';
-import { WormholeComponent } from '../wormhole/wormhole.component';
 
 @Component({
   selector: 'app-cosmic-root',
@@ -15,13 +11,9 @@ import { WormholeComponent } from '../wormhole/wormhole.component';
   imports: [
     ScanningArtifactComponent,
     CosmicStarsComponent,
-    SecretStarComponent,
     CosmicNebulaComponent,
     CosmicGridComponent,
     CosmicBeamComponent,
-    LayerJumpComponent,
-    LayerIndicatorComponent,
-    WormholeComponent
   ],
   templateUrl: './cosmic-root.component.html',
   styleUrls: ['./cosmic-root.component.scss'],
